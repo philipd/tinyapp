f8a4e8dbee44cb7b204906921a46ac89d1652711
Philip Dickau <philip.hd@protonmail.com>
2020-08-31 15:29:22 -0600

    Create infrastructure to support POST messages from input form

--- express-server.js
+++ express-server.js
@@ -1,13 +1,19 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
+
+const generateRandomString = function () {
+
+}
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -30,7 +36,16 @@ app.get('/urls', (req, res) => {
   res.render('urls-index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls-new');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls-show', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
 });
